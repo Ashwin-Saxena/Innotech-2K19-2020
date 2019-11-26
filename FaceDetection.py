2b810737e7f961025a9161ab2344e6350c4b186c
Ashwin Saxena <52866305+Ashwin-Saxena@users.noreply.github.com>
2019-11-26 19:03:02 +0530

    System_Gone_wrong

---
+++ FaceDetection.py
@@ -0,0 +1,20 @@
+import cv2
+import sys
+face_cascade=cv2.CascadeClassifier('C:\\Users\\hp\\Desktop\\frontalFace10\\haarcascade_frontalface_alt.xml')
+video_capture=cv2.VideoCapture(0)
+while True:
+    retval,frame=video_capture.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(35,35)
+
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame ,(x,y) , (x+w,y+h),(50,50,200),2)
+
+    cv2.imshow('Video',frame)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        sys.exit()
